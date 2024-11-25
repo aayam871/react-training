b126b0d0b84a2112863925c16ff95e3442ec982b
Aayam Regmi <aayamregmi81@gmail.com>
2024-11-25 12:24:43 +0545

    initialize our project from react

---
+++ intro.js
@@ -0,0 +1,12 @@
+const child = React.createElement(
+    'div',
+    { id: 'child' },
+    [React.createElement('h1', { id: 'heading' }, 'hello world'),
+    React.createElement('h1', { id: 'heading' }, 'hello world2')
+    ],
+);
+const heading = React.createElement('h1', {}, 'hello world');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(child);
+
+
